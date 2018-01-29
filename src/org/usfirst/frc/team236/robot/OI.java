934ae6959dbac3e91bf05cd06b94d0b3250ccbd6
Sam Fuchs <samcfuchs@gmail.com>
2018-01-29 13:17:22 -0500

    Initial commit

---
+++ src/org/usfirst/frc/team236/robot/OI.java
@@ -0,0 +1,11 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team236.robot;
+
+public class OI {
+}
---
+++ src/org/usfirst/frc/team236/robot/Robot.java
@@ -0,0 +1,65 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team236.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+
+public class Robot extends TimedRobot {
+	
+	// Declare OI
+	public static OI oi;
+	
+	// Declare subsystems
+
+	// Declare auto command
+	Command autoCommand;
+	
+	@Override
+	public void robotInit() {
+		oi = new OI();
+		
+		
+	}
+
+	@Override
+	public void disabledInit() {
+
+	}
+
+	@Override
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	@Override
+	public void autonomousInit() {
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	@Override
+	public void teleopInit() {
+		if (autoCommand != null) {
+			autoCommand.cancel();
+		}
+	}
+
+	@Override
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	@Override
+	public void testPeriodic() {
+	}
+}
