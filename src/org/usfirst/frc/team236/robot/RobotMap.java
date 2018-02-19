79aec111ebbf7e5e363113aea8cf13b21cb04f52
Sam Fuchs <samcfuchs@gmail.com>
2018-02-18 19:42:40 -0500

    Adjust shooting speeds

--- src/org/usfirst/frc/team236/robot/RobotMap.java
+++ src/org/usfirst/frc/team236/robot/RobotMap.java
@@ -46,7 +46,7 @@ public class RobotMap {
 		
 		public static final double INTAKE_SPEED = 1;
 		public static final double EJECT_SPEED = -1;
-		public static final double FEED_SPEED = 0.4;
+		public static final double FEED_SPEED = 0.75;
 		public static final boolean INV_LEFT = true;
 		public static final boolean INV_RIGHT = false;
 
@@ -62,7 +62,7 @@ public class RobotMap {
 		public static final int PWM_BOTTOM_LEFT = 5;
 		public static final int PWM_SPITTER = 6;
 
-		public static final double LAUNCH_SPEED = 0.9;
+		public static final double LAUNCH_SPEED = .85;
 		public static final double SPIT_SPEED = 0.6;
 
 	}
