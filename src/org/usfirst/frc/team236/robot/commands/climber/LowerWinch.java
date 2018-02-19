b17bbfe2084df3255d8f908d98a5701ee08df535
Sam Fuchs <samcfuchs@gmail.com>
2018-02-18 19:33:27 -0500

    Add LowerWinch command

---
+++ src/org/usfirst/frc/team236/robot/commands/climber/LowerWinch.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team236.robot.commands.climber;
+
+import org.usfirst.frc.team236.robot.Robot;
+import org.usfirst.frc.team236.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class LowerWinch extends Command {
+
+    public LowerWinch() {
+    	requires(Robot.climber);
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		Robot.climber.leftWinch.set(-RobotMap.ClimberMap.WINCH_SPEED);
+		Robot.climber.rightWinch.set(-RobotMap.ClimberMap.WINCH_SPEED);
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+		Robot.climber.stopWinch();
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
+}
