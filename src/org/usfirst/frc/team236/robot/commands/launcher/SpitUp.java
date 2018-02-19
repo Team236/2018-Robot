7dff6d88473992f0f529b48f2423a5b38ade7e96
Sam Fuchs <samcfuchs@gmail.com>
2018-02-18 19:46:54 -0500

    Add command to set shooter to spit speed

---
+++ src/org/usfirst/frc/team236/robot/commands/launcher/SpitUp.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team236.robot.commands.launcher;
+
+import org.usfirst.frc.team236.robot.Robot;
+import org.usfirst.frc.team236.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class SpitUp extends Command {
+
+	public SpitUp() {
+		requires(Robot.launcher);
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		Robot.launcher.setLaunchSpeed(RobotMap.LauncherMap.SPIT_SPEED);
+		Robot.launcher.setSpitSpeed(RobotMap.LauncherMap.SPIT_SPEED);
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+		Robot.launcher.stopLaunch();
+		Robot.launcher.stopSpit();
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
+}
