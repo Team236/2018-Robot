9474da324003f8c6847a0b00faed5da963f900d1
Sam Fuchs <samcfuchs@gmail.com>
2018-02-18 19:48:33 -0500

    Add command to raise intake while held

---
+++ src/org/usfirst/frc/team236/robot/commands/intake/RaiseAndLower.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team236.robot.commands.intake;
+
+import org.usfirst.frc.team236.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class RaiseAndLower extends Command {
+
+	public RaiseAndLower() {
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		Robot.intake.raise();
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+		Robot.intake.lower();
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
+}
