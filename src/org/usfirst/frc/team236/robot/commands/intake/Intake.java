4860ad761c516f8f543540089e6c96b8211d4472
Sam Fuchs <samcfuchs@gmail.com>
2018-02-20 15:08:18 -0500

    Remove old smartdashboard prints for intake

--- src/org/usfirst/frc/team236/robot/commands/intake/Intake.java
+++ src/org/usfirst/frc/team236/robot/commands/intake/Intake.java
@@ -4,7 +4,6 @@ import org.usfirst.frc.team236.robot.Robot;
 import org.usfirst.frc.team236.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
@@ -17,20 +16,16 @@ public class Intake extends Command {
 
 	// Called just before this Command runs the first time
 	protected void initialize() {
-
-		SmartDashboard.putBoolean("Initial Intake Sensor value in Intake ", Robot.intake.intakeSensor.get());	
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-
-		SmartDashboard.putBoolean("Intake Sensor value ", Robot.intake.intakeSensor.get());
 		// System.out.print("intake sensor value ");
 		// System.out.println(Robot.intake.intakeSensor.get());
 		
 		if (!Robot.intake.intakeSensor.get()) {
-		Robot.intake.leftIntake.set(RobotMap.IntakeMap.INTAKE_SPEED);
-		Robot.intake.rightIntake.set(RobotMap.IntakeMap.INTAKE_SPEED);
+			Robot.intake.leftIntake.set(RobotMap.IntakeMap.INTAKE_SPEED);
+			Robot.intake.rightIntake.set(RobotMap.IntakeMap.INTAKE_SPEED);
 		}
 
 	}
