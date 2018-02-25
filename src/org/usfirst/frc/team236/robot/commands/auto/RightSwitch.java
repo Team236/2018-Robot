0e8a41b3fde3447d6bf637f2ccb6ac8b6b79a014
Sam Fuchs <samcfuchs@gmail.com>
2018-02-25 16:17:26 -0500

    Add PreAuto to RightSwitch

--- src/org/usfirst/frc/team236/robot/commands/auto/RightSwitch.java
+++ src/org/usfirst/frc/team236/robot/commands/auto/RightSwitch.java
@@ -1,8 +1,6 @@
 package org.usfirst.frc.team236.robot.commands.auto;
 
 import org.usfirst.frc.team236.robot.AutoMap;
-import org.usfirst.frc.team236.robot.commands.intake.Feed;
-import org.usfirst.frc.team236.robot.commands.intake.Lower;
 import org.usfirst.frc.team236.robot.commands.launcher.SpitUpAndShoot;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -19,9 +17,8 @@ public class RightSwitch extends CommandGroup {
 
     public RightSwitch() {
     	//"4" below is a timeout for this command - it means the command will end after those many seconds at most
-    	addParallel(new Lower());
-    	addParallel(new Feed(), .5);
-    	addSequential(new MotionMagic(AutoMap.DIST_FENCE, AutoMap.MM_END_MARGIN2), 4);
+    	addParallel(new PreAuto());
+    	addSequential(new MotionMagic(AutoMap.DIST_FENCE, AutoMap.MM_END_MARGIN2, 100, 100), 4);
     	
     	addSequential(new SpitUpAndShoot());
     }
