cf0b9fe2030408eb4a079523e0b21ba4dae1d5eb
Sam Fuchs <samcfuchs@gmail.com>
2018-03-04 11:41:41 -0500

    Adjust distances for CenterLeftSwitch

--- src/org/usfirst/frc/team236/robot/AutoMap.java
+++ src/org/usfirst/frc/team236/robot/AutoMap.java
@@ -1,9 +1,9 @@
 package org.usfirst.frc.team236.robot;
 
 public class AutoMap {
-	public static final double SWITCH_CROSS1 = -48; // 48
+	public static final double SWITCH_CROSS1 = -42; // 48
 	public static final double SWITCH_CROSS2 = -108; // 108
-	public static final double SWITCH_CROSS3 = -24; // 24+
+	public static final double SWITCH_CROSS3 = -36; // 24+
 
 	public static final double DIST_FENCE = -98; // 102
 	public static final double DIST_FENCE_ALIGNMENT = -30;
