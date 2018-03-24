2b8dd1f1dd6d444584bf903714346cb3d0903874
Sam Fuchs <samcfuchs@gmail.com>
2018-03-24 19:33:14 -0400

    Improve waypoints
    Make sure waypoints represent robot center

--- src/org/usfirst/frc/team236/robot/AutoMap.java
+++ src/org/usfirst/frc/team236/robot/AutoMap.java
@@ -33,25 +33,25 @@ public class AutoMap {
 	public static class Paths {
 		
 		public static final Waypoint[] CENTER_LEFT_SWITCH = new Waypoint[] {
-				new Waypoint(0, 18*12, 0),
-				new Waypoint(5*12, 13*12, Pathfinder.d2r(110)),
-				new Waypoint(12*12, 9*12, 0)
+				new Waypoint(1.5*12, 18*12, 0),
+				//new Waypoint(5*12, 13*12, Pathfinder.d2r(110)),
+				new Waypoint(10.5*12, 9*12, 0)
 		};
 
 		public static final Waypoint[] LEFT_LONG_SCALE = new Waypoint[] {
-				new Waypoint(0, 23*12, 0),
+				new Waypoint(1.5*12, 23*12, 0),
 				new Waypoint(14*12, 23*12, 0),
 				new Waypoint(20*12, 17*12, Pathfinder.d2r(90)),
 				new Waypoint(20*12, 10*12, Pathfinder.d2r(90)),
-				new Waypoint(24*12, 2*12, Pathfinder.d2r(155))
+				new Waypoint(24*12, 2.5*12, Pathfinder.d2r(155))
 		};
 
 		public static final Waypoint[] RIGHT_LONG_SCALE = new Waypoint[] {
-				new Waypoint(0, 4*12, 0),
+				new Waypoint(1.5*12, 4*12, 0),
 				new Waypoint(14*12, 4*12, 0),
 				new Waypoint(20*12, 10*12, Pathfinder.d2r(90)),
 				new Waypoint(20*12, 17*12, Pathfinder.d2r(90)),
-				new Waypoint(24*12, 25*12, Pathfinder.d2r(25))
+				new Waypoint(24*12, 24.5*12, Pathfinder.d2r(25))
 		};
 		
 	}
