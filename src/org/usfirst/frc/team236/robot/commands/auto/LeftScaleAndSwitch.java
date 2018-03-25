72ce1bba0ef19ea2a2223fd50a0987dce42616b9
Sam Fuchs <samcfuchs@gmail.com>
2018-03-25 15:09:23 -0500

    Fix some issues with LeftScale-Switch auto

--- src/org/usfirst/frc/team236/robot/commands/auto/LeftScaleAndSwitch.java
+++ src/org/usfirst/frc/team236/robot/commands/auto/LeftScaleAndSwitch.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team236.robot.commands.auto;
 import org.usfirst.frc.team236.robot.AutoMap;
 import org.usfirst.frc.team236.robot.RobotMap.DriveMap;
 import org.usfirst.frc.team236.robot.commands.intake.Eject;
-import org.usfirst.frc.team236.robot.commands.intake.IntakeAndFeed;
+import org.usfirst.frc.team236.robot.commands.intake.Intake;
 import org.usfirst.frc.team236.robot.commands.intake.Raise;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -15,12 +15,12 @@ public class LeftScaleAndSwitch extends CommandGroup {
 
 	public LeftScaleAndSwitch() {
 		addSequential(new LeftScale());
-		addSequential(new Turn(-AutoMap.TURN_NULL_TO_CUBE, DriveMap.TURN_MARGIN, DriveMap.TURN_PARAMS_90), 1);
+		addSequential(new Turn(-AutoMap.TURN_NULL_TO_CUBE, DriveMap.TURN_MARGIN, DriveMap.TURN_PARAMS_90));
 
 		addParallel(new MotionMagic(AutoMap.DIST_SCALE_TO_CUBE, 3));
-		addSequential(new IntakeAndFeed());
+		addSequential(new Intake());
 
-		addSequential(new Raise(), .5);
+		addSequential(new Raise(), 1);
 		addSequential(new Eject(), .5);
 	}
 
