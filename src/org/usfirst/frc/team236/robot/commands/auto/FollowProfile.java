1ba98bd9ed99903e865dc6e632b588ce4a76f805
Sam Fuchs <samcfuchs@gmail.com>
2018-03-31 13:55:12 -0500

    Miscellaneous fixes to FollowProfile command

--- src/org/usfirst/frc/team236/robot/commands/auto/FollowProfile.java
+++ src/org/usfirst/frc/team236/robot/commands/auto/FollowProfile.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team236.robot.commands.auto;
 import org.usfirst.frc.team236.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import lib.motionProfile.DriveParameters;
 import lib.motionProfile.Element;
 import lib.motionProfile.TrapProfile;
@@ -15,6 +16,7 @@ public class FollowProfile extends Command {
 	TrapProfile profile;
 	DriveParameters params;
 	boolean reverse;
+	int reverseMultiplier = 1;
 	double endPos, margin, leftDistToEnd, rightDistToEnd;
 	int i = 0;
 
@@ -26,6 +28,10 @@ public class FollowProfile extends Command {
 		this.reverse = _rev;
 		this.margin = _margin;
 		this.endPos = profile.getLast().position;
+
+		if (reverse) {
+			this.reverseMultiplier = -1;
+		}
 	}
 
 	protected void initialize() {
@@ -44,27 +50,30 @@ public class FollowProfile extends Command {
 
 		// Get element from profile
 		Element e = profile.get(i);
-		
+
 		// Calculate feedforwards
-		double l_v = params.kV * e.speed;
+		double l_v = params.kV_l * e.speed;
 		double l_a = params.kA * e.acceleration;
 		// Calculate corrections
-		double l_error = (e.position - Robot.drive.getLeftDistance());
+		double l_error = (reverseMultiplier * e.position - Robot.drive.getLeftDistance());
 		double l_p = params.kP * l_error;
-		leftDistToEnd = Robot.drive.getLeftDistance() - endPos;
-		
+		leftDistToEnd = reverseMultiplier * Robot.drive.getLeftDistance() - endPos;
+
 		// Repeat for right side
 		// Calculate feedforwards
-		double r_v = params.kV * e.speed;
+		double r_v = params.kV_r * e.speed;
 		double r_a = params.kA * e.acceleration;
 		// Calculate corrections
-		double r_error = (e.position - Robot.drive.getLeftDistance());
+		double r_error = (reverseMultiplier * e.position - Robot.drive.getRightDistance());
 		double r_p = params.kP * r_error;
-		rightDistToEnd = Robot.drive.getRightDistance() - endPos;
-		
-		Robot.drive.setLeftSpeed(l_v + l_a + l_p);
-		Robot.drive.setRightSpeed(r_v + r_a + r_p);
-		
+		rightDistToEnd = reverseMultiplier * Robot.drive.getRightDistance() - endPos;
+
+		Robot.drive.setLeftSpeed(reverseMultiplier * (l_v + l_a + l_p));
+		Robot.drive.setRightSpeed(reverseMultiplier * (r_v + r_a + r_p));
+
+		SmartDashboard.putNumber("Right profile error", r_error);
+		SmartDashboard.putNumber("Left profile error", l_error);
+
 		i++;
 	}
 
