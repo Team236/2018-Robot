4ddcdbdbca8a83ced1a34bdad6fe1da84da0bc1d
Sam Fuchs <samcfuchs@gmail.com>
2018-03-31 23:17:51 -0400

    Add speed input to spinupandshoot
    Refactor with new shooter commands

--- src/org/usfirst/frc/team236/robot/commands/launcher/SpinUpAndShoot.java
+++ src/org/usfirst/frc/team236/robot/commands/launcher/SpinUpAndShoot.java
@@ -3,16 +3,28 @@ package org.usfirst.frc.team236.robot.commands.launcher;
 import org.usfirst.frc.team236.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import lib.commands.Wait;
 import lib.commands.WaitCommand;
 
 /**
  *
  */
 public class SpinUpAndShoot extends CommandGroup {
+	
+	public SpinUpAndShoot(double speed) {
+		/*
+		addParallel(new WaitCommand(RobotMap.LauncherMap.SPINUP_TIME, new Shoot(), RobotMap.LauncherMap.SHOOT_TIME));
 
-    public SpinUpAndShoot() {
-    	addParallel(new WaitCommand(RobotMap.LauncherMap.SPINUP_TIME, new Shoot(), RobotMap.LauncherMap.SHOOT_TIME));
-    	
-    	addSequential(new SpinUp(), RobotMap.LauncherMap.SPINUP_TIME + RobotMap.LauncherMap.SHOOT_TIME);
-    }
+		addSequential(new SpinUp(speed), RobotMap.LauncherMap.SPINUP_TIME + RobotMap.LauncherMap.SHOOT_TIME);
+		*/
+		addSequential(new SpinUpNoStop(speed));
+		addSequential(new Wait(RobotMap.LauncherMap.SPINUP_TIME));
+		
+		addSequential(new Shoot(), RobotMap.LauncherMap.SHOOT_TIME);
+		addSequential(new SpinDown());
+	}
+	
+	public SpinUpAndShoot() {
+		this(RobotMap.LauncherMap.LAUNCH_SPEED);
+	}
 }
