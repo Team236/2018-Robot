48a8fde37ae17046e919f3460afe027aa41c3bcc
Sam Fuchs <samcfuchs@gmail.com>
2018-03-31 22:38:38 -0400

    Add new commands to left scale to shoot faster

--- src/org/usfirst/frc/team236/robot/commands/auto/left/LeftScale.java
+++ src/org/usfirst/frc/team236/robot/commands/auto/left/LeftScale.java
@@ -1,11 +1,15 @@
 package org.usfirst.frc.team236.robot.commands.auto.left;
 
 import org.usfirst.frc.team236.robot.Robot;
+import org.usfirst.frc.team236.robot.RobotMap;
 import org.usfirst.frc.team236.robot.RobotMap.DriveMap;
 import org.usfirst.frc.team236.robot.commands.auto.FollowProfile;
 import org.usfirst.frc.team236.robot.commands.auto.PreAuto;
 import org.usfirst.frc.team236.robot.commands.auto.Turn;
+import org.usfirst.frc.team236.robot.commands.launcher.Shoot;
+import org.usfirst.frc.team236.robot.commands.launcher.SpinDown;
 import org.usfirst.frc.team236.robot.commands.launcher.SpinUpAndShoot;
+import org.usfirst.frc.team236.robot.commands.launcher.SpinUpNoStop;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -26,9 +30,16 @@ public class LeftScale extends CommandGroup {
 		//addSequential(new MotionMagic(AutoMap.DIST_NULL_TERRITORY, AutoMap.MM_END_MARGIN, 350, 800));
 		addSequential(new FollowProfile(Robot.scale, true));
 		
+		addParallel(new SpinUpNoStop());
 		addSequential(new Turn(60, DriveMap.TURN_MARGIN, DriveMap.TURN_PARAMS_45)); // could angle robot and turn 60
 
-		addSequential(new SpinUpAndShoot());
+		//addSequential(new SpinUpAndShoot());
+
+		addSequential(new Shoot(), RobotMap.LauncherMap.SHOOT_TIME);
+
+		addSequential(new SpinDown());
+
+		//addSequential(new SpinUpAndShoot());
 	}
 
 	@Override
