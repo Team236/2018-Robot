87a89a8aabaf0347442ffa67d98ddba3b7bf464c
Sam Fuchs <samcfuchs@gmail.com>
2018-04-08 10:57:21 -0400

    Change shooter delays

--- src/org/usfirst/frc/team236/robot/RobotMap.java
+++ src/org/usfirst/frc/team236/robot/RobotMap.java
@@ -121,8 +121,8 @@ public class RobotMap {
 		public static final double SPIT_SPEED = 0.5;
 		public static final double FEED_SPEED = -0.4;
 
-		public static final double SPINUP_TIME = .40;
-		public static final double SHOOT_TIME = .50;
+		public static final double SPINUP_TIME = .3;
+		public static final double SHOOT_TIME = .25;
 	}
 
 	public static class ClimberMap {
