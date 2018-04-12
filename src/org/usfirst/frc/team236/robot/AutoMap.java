56c6a29f645310ffa37bf5d2244f2779777296a8
Sam Fuchs <samcfuchs@gmail.com>
2018-04-12 18:22:29 -0400

    Tune auto distances and parameters

--- src/org/usfirst/frc/team236/robot/AutoMap.java
+++ src/org/usfirst/frc/team236/robot/AutoMap.java
@@ -10,7 +10,7 @@ public class AutoMap {
 	public static final double SWITCH_CROSS2 = -108; // 108
 	public static final double SWITCH_CROSS3 = -67; // 24+
 
-	public static final double DIST_FENCE = -110; // 102
+	public static final double DIST_FENCE = -102; // 102
 	public static final double DIST_FENCE_ALIGNMENT = -30;
 	public static final double DIST_PAST_SWITCH = -140; // 168
 	public static final double DIST_SIDE_FENCE = -30; // 21+
@@ -22,17 +22,17 @@ public class AutoMap {
 	public static final double DIST_CROSS_FIELD = -240; // 186 with bump, try 210
 	public static final double DIST_PLATFORM_ZONE_TO_NULL_TERRITORY = -54; // 37
 
-	public static final double DIST_PLATFORM_ZONE_DIRECT = -220;
-	public static final double DIST_CROSS_FIELD_DIRECT = 303; // -240, 186 with bump, try 210
+	public static final double DIST_PLATFORM_ZONE_DIRECT = -248;//-220
+	public static final double DIST_CROSS_FIELD_DIRECT = 255; //303 -240, 186 with bump, try 210
 
 	public static final double DIST_AUTO_LINE = -150;
 
-	public static final double DIST_SCALE_TO_CUBE = 66; // 70
+	public static final double DIST_SCALE_TO_CUBE = 70; // 70
 
 	public static final double MM_END_MARGIN = 10;
 
-	public static final ProfileParameters TO_NULL = new ProfileParameters(-DIST_NULL_TERRITORY, 100, 200, 0, 3);
-	public static final ProfileParameters STRAIGHT_SWITCH = new ProfileParameters(-DIST_FENCE, 80, 160, 0, 4);
+	public static final ProfileParameters TO_NULL = new ProfileParameters(-DIST_NULL_TERRITORY, 100, 100, 0, 3);
+	public static final ProfileParameters STRAIGHT_SWITCH = new ProfileParameters(-DIST_FENCE, 50, 50, 0, 4);
 	public static final ProfileParameters TO_CUBE1 = new ProfileParameters(-DIST_SCALE_TO_CUBE, 50, 100, 0, 4);
 
 	public static final ProfileParameters OUTER = new ProfileParameters(-DIST_PAST_SWITCH, 150, 300, 0, 8, 1 / 100.0);
