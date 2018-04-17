20972b484f53552e1effd4ec5508c791bb599bda
Sam Fuchs <samcfuchs@gmail.com>
2018-04-16 22:23:03 -0400

    Update/fix right side autos

--- src/org/usfirst/frc/team236/robot/commands/auto/right/RightScale.java
+++ src/org/usfirst/frc/team236/robot/commands/auto/right/RightScale.java
@@ -1,11 +1,13 @@
 package org.usfirst.frc.team236.robot.commands.auto.right;
 
-import org.usfirst.frc.team236.robot.AutoMap;
+import org.usfirst.frc.team236.robot.Robot;
 import org.usfirst.frc.team236.robot.RobotMap.DriveMap;
-import org.usfirst.frc.team236.robot.commands.auto.MotionMagic;
+import org.usfirst.frc.team236.robot.commands.auto.FollowProfile;
 import org.usfirst.frc.team236.robot.commands.auto.PreAuto;
 import org.usfirst.frc.team236.robot.commands.auto.Turn;
+import org.usfirst.frc.team236.robot.commands.launcher.SpinDown;
 import org.usfirst.frc.team236.robot.commands.launcher.SpinUpAndShoot;
+import org.usfirst.frc.team236.robot.commands.launcher.SpinUpNoStop2;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -21,11 +23,15 @@ public class RightScale extends CommandGroup {
 	 */
 	public RightScale() {
 		addParallel(new PreAuto());
-		addSequential(new MotionMagic(AutoMap.DIST_NULL_TERRITORY, AutoMap.MM_END_MARGIN));
+		//addSequential(new MotionMagic(AutoMap.DIST_NULL_TERRITORY, AutoMap.MM_END_MARGIN));
+		addSequential(new FollowProfile(Robot.scale, true));
 
+		addParallel(new SpinUpNoStop2(0.95));
 		addSequential(new Turn(-60, DriveMap.TURN_MARGIN, DriveMap.TURN_PARAMS_45));
 
 		addSequential(new SpinUpAndShoot());
+		
+		addSequential(new SpinDown());
 	}
 
 	@Override
--- src/org/usfirst/frc/team236/robot/commands/auto/right/RightScaleAndSwitch.java
+++ src/org/usfirst/frc/team236/robot/commands/auto/right/RightScaleAndSwitch.java
@@ -4,8 +4,9 @@ import org.usfirst.frc.team236.robot.AutoMap;
 import org.usfirst.frc.team236.robot.RobotMap.DriveMap;
 import org.usfirst.frc.team236.robot.commands.auto.MotionMagic;
 import org.usfirst.frc.team236.robot.commands.auto.Turn;
+import org.usfirst.frc.team236.robot.commands.drive.StraightDrive;
 import org.usfirst.frc.team236.robot.commands.intake.Eject;
-import org.usfirst.frc.team236.robot.commands.intake.IntakeAndFeed;
+import org.usfirst.frc.team236.robot.commands.intake.Intake;
 import org.usfirst.frc.team236.robot.commands.intake.Raise;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -17,12 +18,16 @@ public class RightScaleAndSwitch extends CommandGroup {
 
 	public RightScaleAndSwitch() {
 		addSequential(new RightScale());
-		addSequential(new Turn(-AutoMap.TURN_NULL_TO_CUBE, DriveMap.TURN_MARGIN, DriveMap.TURN_PARAMS_90));
 
-		addParallel(new IntakeAndFeed());
-		addSequential(new MotionMagic(AutoMap.DIST_SCALE_TO_CUBE, 3));
+		addSequential(new Turn(AutoMap.TURN_NULL_TO_CUBE, DriveMap.TURN_MARGIN, DriveMap.TURN_PARAMS_90));
 
-		addSequential(new Raise(), .5);
+		addParallel(new MotionMagic(AutoMap.DIST_SCALE_TO_CUBE, 3));
+		addSequential(new Intake());
+		
+		addSequential(new Raise(), 1);
+
+		addSequential(new StraightDrive(.3), 1);
+
 		addSequential(new Eject(), .5);
 	}
 
